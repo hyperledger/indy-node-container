42a91f8cde85a03327ab30c2fa3f1bba6298f7a1
Sebastian Schmittner <schmittner@eecc.info>
2020-08-31 15:19:12 +0200

    indy node 1.12.3 and cli + webterminal running
    Signed-off-by: Sebastian Schmittner <sebastian.schmittner@eecc.de>

--- generate_random_seeds.sh
+++ generate_random_seeds.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 echo -e "[...]\t storing random seeds in .cli.env and .node.env"
+echo "INDY_NODE_SEED=$(head -c 32 /dev/random | base64 | head -c 32)" > .node.env
 echo "INDY_STEWARD_SEED=$(head -c 32 /dev/random | base64 | head -c 32)" > .cli.env
-echo "INDY_NODE_SEED==$(head -c 32 /dev/random | base64 | head -c 32)" > .node.env
+echo "WALLET_KEY=$(head -c 32 /dev/random | base64 | head -c 32)" >> .cli.env
+echo "INDY_ENDORSER_SEED=$(head -c 32 /dev/random | base64 | head -c 32)" >> .cli.env
 echo -e "[OK]\t done"
