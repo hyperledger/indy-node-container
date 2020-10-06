831ecc373b4602886cd75eb1126305c6d7412d01
Sebastian Schmittner <schmittner@eecc.info>
2020-10-06 12:28:29 +0000

    removed seed gen and readme for indy_cli
    Signed-off-by: Sebastian Schmittner <sebastian.schmittner@eecc.de>

--- generate_random_seeds.sh
+++ generate_random_seeds.sh
@@ -2,7 +2,4 @@
 
 echo -e "[...]\t storing random seeds in .cli.env and .node.env"
 echo "INDY_NODE_SEED=$(head -c 32 /dev/random | base64 | head -c 32)" > .node.env
-echo "INDY_STEWARD_SEED=$(head -c 32 /dev/random | base64 | head -c 32)" > .cli.env
-echo "WALLET_KEY=$(head -c 32 /dev/random | base64 | head -c 32)" >> .cli.env
-echo "INDY_ENDORSER_SEED=$(head -c 32 /dev/random | base64 | head -c 32)" >> .cli.env
 echo -e "[OK]\t done"
