5ee66848a66e224843316b15d6562a5ed9f0b61f
Sebastian Schmittner <schmittner@eecc.info>
2021-02-12 10:10:22 +0000

    scripts to set up port 9701 whitelist and 9702 connlimits for docker container. Example files using IDU IPs

---
+++ set_iptables_for_idu.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -x
+
+
+# skip existing rules to avoid duplicates
+add_new_rule()
+{
+    RULE="$@"
+
+    if ! sudo iptables -C $RULE 2>/dev/null 1>&2; then
+	sudo iptables -I $RULE
+    fi
+}
+
+
+# 9701 whitelist approach: drop all others
+add_new_rule DOCKER-USER -p tcp --dport 9701 -j DROP
+
+# 9701 create IP whitelist from file
+while read IP; do
+    add_new_rule DOCKER-USER -p tcp --dport 9701 -s $IP -j ACCEPT
+done < idu_ips
+
+# 9702 connlimit
+add_new_rule DOCKER-USER -p tcp --syn --dport 9702 -m connlimit --connlimit-above 16 -j REJECT
+
