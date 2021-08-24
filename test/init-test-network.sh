d2ff3eab5304b9e9bb5cb7b6bc8811fa249ce118
Sebastian Schmittner <sebastian.schmittner@eecc.de>
2021-08-24 09:35:20 +0200

    introduced IMAGE .env variable to test setup (working on #35)

--- test/init-test-network.sh
+++ test/init-test-network.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-# inits 4 nodes for a local test network 
+# inits 4 nodes for a local test network
+source .env
 mkdir -p lib_indy
-docker run -v ${PWD}/etc_indy:/etc/indy -v ${PWD}/lib_indy:/var/lib/indy ghcr.io/idunion/indy-node-container/indy_node:buster \
-    /bin/bash -c "rm -rf /var/lib/indy/* && generate_indy_pool_transactions --nodes 4 --clients 0 --nodeNum 1 2 3 4 --ips=\"10.133.133.1,10.133.133.2,10.133.133.3,10.133.133.4\" --network idunion_local_test"
+docker run -v ${PWD}/etc_indy:/etc/indy -v ${PWD}/lib_indy:/var/lib/indy $IMAGE \
+    /bin/bash -c "rm -rf /var/lib/indy/* && generate_indy_pool_transactions --nodes 4 --clients 0 --nodeNum 1 2 3 4 --ips=\"10.133.133.1,10.133.133.2,10.133.133.3,10.133.133.4\" --network $INDY_NETWORK_NAME"
