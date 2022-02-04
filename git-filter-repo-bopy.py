390b49906c7c854c3dbcd8f870031e1b7e2651fd
Sebastian Schmittner <sebastian.schmittner@eecc.de>
2022-02-04 11:28:22 +0100

    adding script to rewrite git history adding commit sign offs
    Signed-off-by: Sebastian Schmittner <sebastian.schmittner@eecc.de>

---
+++ git-filter-repo-bopy.py
@@ -0,0 +1,12 @@
+  # This file is supposed to be used as `git filter-repo --commit-callback '$(cat git-filter-repo-body.py)'`
+  # See https://htmlpreview.github.io/?https://github.com/newren/git-filter-repo/blob/docs/html/git-filter-repo.html#CALLBACKS
+  # 
+  # * Commit: `branch`, `original_id`, `author_name`, `author_email`,
+  #         `author_date`, `committer_name`, `committer_email`,
+  #         `committer_date`, `message`, `file_changes` (list of
+  #         FileChange objects, each containing a `type`, `filename`,
+  #         `mode`, and `blob_id`), `parents` (list of hashes or integer
+  #         marks)
+  if commit.author_email == b"sebastian.schmittner@eecc.de" and b"Signed-off-by:" not in commit.message:
+      commit.message = commit.message + b"\nSigned-off-by: Sebastian Schmittner <sebastian.schmittner@eecc.de>"
+  
