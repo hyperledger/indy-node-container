ea6ee471c31e2191963009c277d40f7d85e27c80
Sebastian Schmittner <sebastian.schmittner@eecc.de>
2022-02-04 14:12:04 +0100

    histroy rewrite script for sign offs improved
    Signed-off-by: Sebastian Schmittner <sebastian.schmittner@eecc.de>

--- git-filter-repo-bopy.py
+++ git-filter-repo-bopy.py
@@ -1,4 +1,7 @@
-  # This file is supposed to be used as `git filter-repo --commit-callback '$(cat git-filter-repo-body.py)'`
+  # This file is supposed to be used as `git filter-repo --force --commit-callback "$(cat git-filter-repo-body.py)"`
+  # Mind to adapt the following before:
+  username = b"Sebastian Schmittner"
+  email = b"sebastian.schmittner@eecc.de"
   # See https://htmlpreview.github.io/?https://github.com/newren/git-filter-repo/blob/docs/html/git-filter-repo.html#CALLBACKS
   # 
   # * Commit: `branch`, `original_id`, `author_name`, `author_email`,
@@ -7,6 +10,5 @@
   #         FileChange objects, each containing a `type`, `filename`,
   #         `mode`, and `blob_id`), `parents` (list of hashes or integer
   #         marks)
-  if commit.author_name == b"Sebastian Schmittner" and b"Signed-off-by:" not in commit.message:
-      commit.message = commit.message + b"\n\nSigned-off-by: Sebastian Schmittner <sebastian.schmittner@eecc.de>"
-  
+  if commit.author_name == username and b"Signed-off-by:" not in commit.message:
+      commit.message = commit.message + b"\n\nSigned-off-by: " + username + " <" + email + ">"
