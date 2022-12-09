068cdee1d4ba8ef4d0b4ea8a3bc821122d603bd8
Sebastian Schmittner <sebastian.schmittner@eecc.de>
2022-12-09 10:06:09 +0100

    revert env variables overriding conf
    Signed-off-by: Sebastian Schmittner <sebastian.schmittner@eecc.de>

--- build/init_and_run.sh
+++ build/init_and_run.sh
@@ -16,24 +16,12 @@ echo "INDY_NODE_IP=${INDY_NODE_IP:=0.0.0.0}"
 echo "INDY_NODE_PORT=${INDY_NODE_PORT:=9701}"
 echo "INDY_CLIENT_IP=${INDY_CLIENT_IP:=0.0.0.0}"
 echo "INDY_CLIENT_PORT=${INDY_CLIENT_PORT:=9702}"
+
 echo "INDY_NODE_SEED=[$(echo -n $INDY_NODE_SEED|wc -c) characters]"
-echo "INDY_NODE_LOG_LEVEL=${INDY_NODE_LOG_LEVEL:=0}"
-echo "INDY_NODE_LOG_STDOUT=${INDY_NODE_LOG_STDOUT:=True}"
-echo "INDY_NODE_LOG_DIR=${INDY_NODE_LOG_DIR:=/var/log/indy}"
 
 # Set NETWORK_NAME in indy_config.py
 awk '{if (index($1, "NETWORK_NAME") != 0) {print("NETWORK_NAME = \"'$INDY_NETWORK_NAME'\"")} else print($0)}' /etc/indy/indy_config.py> /tmp/indy_config.py
 mv /tmp/indy_config.py /etc/indy/indy_config.py
-
-awk '{if (index($1, "enableStdOutLogging") != 0) {print("enableStdOutLogging = '$INDY_NODE_LOG_STDOUT'")} else print($0)}' /etc/indy/indy_config.py> /tmp/indy_config.py
-mv /tmp/indy_config.py /etc/indy/indy_config.py
-
-awk '{if (index($1, "LOG_DIR") != 0) {print("LOG_DIR = \"'$INDY_NODE_LOG_DIR'\"")} else print($0)}' /etc/indy/indy_config.py> /tmp/indy_config.py
-mv /tmp/indy_config.py /etc/indy/indy_config.py
-
-# Append logLevel key if not exists and set it with value from env
-grep -qxF 'logLevel"' /etc/indy/indy_config.py || printf "\n# Defines the LogLevel of IndyNode \nlogLevel = %s\n" "$INDY_NODE_LOG_LEVEL" >> /etc/indy/indy_config.py
-
 
 # Init indy-node
 if [[ ! -d "/var/lib/indy/$INDY_NETWORK_NAME/keys" ]]
@@ -63,7 +51,6 @@ echo -e "[OK]\t Setting directory owner to indy"
 echo -e "[...]\t Starting Indy Node as indy user"
 
 echo
-
 
 exec su indy <<EOF
 start_indy_node "$INDY_NODE_NAME" "$INDY_NODE_IP" "$INDY_NODE_PORT" "$INDY_CLIENT_IP" "$INDY_CLIENT_PORT"
